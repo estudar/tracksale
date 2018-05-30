607b78dda0cabb591a1f303f0530edb0057e210b
Regis Mesquita <publicgh@sunrise.33mail.com>
2018-05-30 10:37:27 -0300

    Campaign acessors

--- lib/tracksale/campaign.rb
+++ lib/tracksale/campaign.rb
@@ -1,14 +1,34 @@
 module Tracksale
   class Campaign
+    attr_accessor :name, :code, :score
+
     def self.find_by_name(name)
-      all.keep_if{|campaign| campaign['name'] == name }
+      campaign_found_by_name = raw_all.keep_if{|campaign| campaign['name'] == name }.first
+      return nil if campaign_found_by_name.nil?
+      create_from_response(campaign_found_by_name)
     end
 
     def self.all
-      client.get('campaign')
+      raw_all.map{ |campaign| create_from_response(campaign) }
+    end
+
+    def self.create_from_response(raw_reponse)
+      self.new.tap{ |campaign|
+        campaign.name = raw_reponse['name']
+        campaign.code = raw_reponse['code']
+        campaign.score = {
+          detractors: raw_reponse['detractors'],
+          passives: raw_reponse['passives'],
+          promoters: raw_reponse['promoters']
+        }
+      }
     end
 
     private
+
+    def self.raw_all
+      client.get('campaign')
+    end
 
     def self.client
       Tracksale::Client.new
--- test/test_tracksale_campaign.rb
+++ test/test_tracksale_campaign.rb
@@ -10,12 +10,45 @@ class TracksaleCampaignTest < Minitest::Test
 
     stub_request(:get, "http://api.tracksale.co/v2/campaign").
       with(headers: { 'authorization' => 'bearer foobar' }).
-      to_return(body: "[{\"name\":\"random - name\"}]", headers: {content_type: 'application/json'}, status: 200)
+      to_return(body: "[{"+
+                      "\"name\":\"random - name\","+
+                      "\"code\":1234,"+
+                      "\"detractors\":1,"+
+                      "\"passives\":2,"+
+                      "\"promoters\":3"+
+                      "}]", headers: {content_type: 'application/json'}, status: 200)
   end
 
   def test_return_right_amount_of_items
     assert_equal 1, Tracksale::Campaign.all.size
-    assert_equal 1, Tracksale::Campaign.find_by_name('random - name').size
-    assert_equal 0, Tracksale::Campaign.find_by_name('random - another name').size
+    assert !Tracksale::Campaign.find_by_name('random - name').nil?
+    assert Tracksale::Campaign.find_by_name('random - another name').nil?
   end
+
+  def test_find_return_a_campaign
+    assert Tracksale::Campaign.find_by_name('random - name').is_a? Tracksale::Campaign
+  end
+
+  def test_campaign_has_a_name
+    assert Tracksale::Campaign.find_by_name('random - name').respond_to? :name
+    assert_equal 'random - name',Tracksale::Campaign.find_by_name('random - name').name
+  end
+
+   def test_campaign_has_a_code
+    assert Tracksale::Campaign.find_by_name('random - name').respond_to? :code
+    assert_equal 1234,Tracksale::Campaign.find_by_name('random - name').code
+   end
+
+   def test_campaign_scores
+     campaign = Tracksale::Campaign.find_by_name('random - name')
+     assert campaign.respond_to? :score
+     expected_score = { detractors: 1, passives: 2, promoters: 3 }
+     #require 'byebug'
+     #byebug
+     assert_equal expected_score, campaign.score
+   end
+
+   def test_all_returns_campaigns
+     assert Tracksale::Campaign.all.first.is_a? Tracksale::Campaign
+   end
 end
